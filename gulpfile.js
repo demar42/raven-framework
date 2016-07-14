35c4bf671eb9c8051e0fc4e4c9fd46562a8679e2
Thomas Higginbotham <thomas.higginbotham@experis.com>
2016-07-14 16:05:54 -0400

    Fix linting issues

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var sass = require('gulp-ruby-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var rename = require('gulp-rename');
 
-gulp.task('sass:dev', function() {
+gulp.task('sass:dev', function () {
 	return sass('./scss/**/*.scss', {style: 'expanded', sourcemap: true})
 		.on('error', sass.logError)
 		.pipe(sourcemaps.write('maps', {
@@ -15,7 +15,7 @@ gulp.task('sass:dev', function() {
 		.pipe(gulp.dest('css'));
 });
 
-gulp.task('sass:dist', function() {
+gulp.task('sass:dist', function () {
 	return sass('scss/**/*.scss', {style: 'compressed', sourcemap: true})
 		.on('error', sass.logError)
 		.pipe(rename({
@@ -32,6 +32,6 @@ gulp.task('default', ['sass:dist']);
 
 gulp.task('watch', ['sass:dev', 'sass:watch']);
 
-gulp.task('sass:watch', function() {
+gulp.task('sass:watch', function () {
 	gulp.watch('./scss/**/*.scss', ['sass:dev']);
 });
